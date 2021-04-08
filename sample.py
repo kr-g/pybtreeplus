33c9a0d174cbe70caf407115362b1dfc1d895e09
kr-g <k.r.goger@gmail.com>
2021-04-08 13:03:32 +0200

    clean up

--- sample.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from tests.test_default import *
-
-suite = unittest.TestLoader().loadTestsFromName(
-    "test_insert_split_leaf_2", module=BTreePlusDefaultTestCase
-)
-unittest.TextTestRunner().run(suite)
